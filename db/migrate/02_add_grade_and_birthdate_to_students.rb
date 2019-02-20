97875c853b8dc0cfaa9140511680ec02e4600465
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 16:07:22 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
-02_add_grade_and_birthdate_to_students.rb
+class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[5.1]
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
